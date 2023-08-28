a07d7096fbc145f04ad3b6f3fad1daa8961d18b5
chenzhiquan <uiopsczc@163.com>
2023-08-28 22:34:36 +0800

    添加luban倒表工具

---
+++ Luban/gen.bat
@@ -0,0 +1,14 @@
+set WORKSPACE=..
+set LUBAN_DLL=%WORKSPACE%\Luban\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -c cs-simple-json ^
+    -d json ^
+    --schemaPath %CONF_ROOT%\MiniTemplate\Defines\__root__.xml ^
+    -x inputDataDir=%CONF_ROOT%\MiniTemplate\Datas  ^
+    -x outputCodeDir=%WORKSPACE%\Assets\Luban\AutoGen\Code  ^
+    -x outputDataDir=%WORKSPACE%\Assets\Luban\AutoGen\Data\json
+    
+pause
